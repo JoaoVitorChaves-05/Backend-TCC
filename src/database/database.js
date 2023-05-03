5e365421d978979022663df1007417060f13f6d6
JoaoVitorChaves-05 <joaovmanciochaves@gmail.com>
2023-05-03 14:32:55 -0300

    update

--- src/database/database.js
+++ src/database/database.js
@@ -16,6 +16,7 @@ class Database {
     }
 
     async createTables() {
+
         this.models.Users = this.connection.define('Users', {
             user_id: {
                 type: DataTypes.INTEGER,
@@ -38,6 +39,8 @@ class Database {
             }
         })
 
+        await this.models.Users.sync()
+
         this.models.Groups = this.connection.define('Groups', {
             group_id: {
                 type: DataTypes.INTEGER,
@@ -49,6 +52,102 @@ class Database {
             }
         })
 
-        this.models.Cameras = this.connection.define('Cameras')
+        await this.models.Groups.sync()
+
+        this.models.Cameras = this.connection.define('Cameras', {
+            camera_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true
+            },
+            group_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: this.models.Groups,
+                    key: 'group_id'
+                }
+            },
+            camera_name: {
+                type: DataTypes.STRING,
+                allowNull: true
+            }
+        })
+
+        await this.models.Cameras.sync()
+
+        this.models.Admins = this.connection.define('Admins', {
+            admin_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: this.models.Users,
+                    key: 'user_id'
+                }
+            },
+            group_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: this.models.Groups,
+                    key: 'group_id'
+                }
+            }
+        })
+
+        await this.models.Admins.sync()
+
+        this.models.Authorized = this.connection.define('Authorized', {
+            authorized_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            group_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: this.models.Groups,
+                    key: 'group_id'
+                }
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: this.models.Users,
+                    key: 'user_id'
+                }
+            }
+        })
+
+        await this.models.Authorized.sync()
+
+        this.models.Biometry = this.connection.define('Biometry', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: this.models.Users,
+                    key: 'user_id'
+                }
+            },
+            biometry_path: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        })
+
+        await this.models.Biometry.sync()
+
     }
 }
+
+export default new Database()
