83069ddf538047bcd287b5105628e6e2b5a00e85
João Vitor Chaves <joaovmanciochaves@gmail.com>
2023-06-25 19:40:21 -0300

    update in python service

--- src/services/app.py
+++ src/services/app.py
@@ -1,36 +1,67 @@
 import cv2
 import face_recognition as fr
-import os
+import mysql.connector
 
 class App:
 
     def __init__(self):
         self.encodings = {}
+        self.photos = {}
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            port='3306',
+            user='root',
+            password='root',
+            database='security_system'
+        )
+    
+    def add_encoding(self, user_data):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM Photo WHERE user_id = {user_data.user_id}")
+
+        result = cursor.fetchOne()
+        photo = fr.load_image_file(result.photo_path)
+        try:
+            encode_photo = fr.face_encodings(photo)[0]
+            if (user_data.group_id in self.encodings.values()):
+                self.encodings[user_data.group_id].append(encode_photo)
+            else:
+                self.encodings[user_data.group_id] = []
+                self.encodings[user_data.group_id].append(encode_photo)
+        except:
+            pass
 
     def load_all_encodings(self):
-        path = '../../images/users/'
-        dirs = os.listdir(path)
-
-        for dir in dirs:
-            print('Loading image: ', dir)
-            photo = fr.load_image_file(path + dir)
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT a.user_id, a.group_id, p.photo_path FROM Authorized as a JOIN Photo as p ON a.user_id = p.user_id;")
+        
+        result = cursor.fetchAll()
+        for row in result:
+            photo_path = row.photo_path
+            print("Loading image: ", photo_path)
+            photo = fr.load_image_file(photo_path)
             try:
                 encode_photo = fr.face_encodings(photo)[0]
-                self.encodings[photo+dir] = encode_photo
+                if (row.group_id in self.encodings.values()):
+                    self.encodings[row.group_id].append(encode_photo)
+                else:
+                    self.encodings[row.group_id] = []
+                    self.encodings[row.group_id].append(encode_photo)
             except:
                 pass
+        cursor.close()
         
     def load_user_encodings(file):
         photo = fr.load_image_file(file)
         encode_photo = fr.face_encodings(photo)[0]
         return encode_photo
     
-    def compare_encodings(user_encoding, encondings):
-        for encoding in encodings:
-            compare = fr.compare_faces([user_encoding], encodings)
+    def compare_encodings(self, user_encoding, group_id):
+        for encoding in self.encodings[group_id]:
+            compare = fr.compare_faces([user_encoding], encoding)
             if compare[0] == True:
                 return True
-            return False
+        return False
 
 app = App()
 app.load_all_encodings()
--- src/services/index.py
+++ src/services/index.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 import app as app_controller
+from threading import Thread
+
 app = Flask(__name__)
 
 @app.route('/face', methods=['POST'])
@@ -14,5 +16,11 @@ def biomether_route():
     if request.method == 'POST':
         data = request.form.get('data')
         return 'Rota /biomether'
-
+    
+@app.route('/add_encoding', methods=['POST'])
+def add_encoding_route():
+    if request.method == 'POST':
+        data = request.form.get('data')
+        app_controller.app.add_encoding(data)
+proccess_photos = Thread(target=app_controller.app.load_all_encodings())
 app.run(port=8080)
